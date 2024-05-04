c05c3b7ac851a2a5d608939a237a6593dff09872
Niestrat99 <niestrat99@gmail.com>
2024-05-04 18:11:05 +0200

    feat: Some funny stuff and a little change in version.

---
+++ src/main/java/io/github/niestrat99/chatsplus/utils/MutualCheck.java
@@ -0,0 +1,31 @@
+package io.github.niestrat99.chatsplus.utils;
+
+import io.github.niestrat99.chatsplus.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class MutualCheck {
+
+    public static void checkForMutuals() {
+        try {
+            HashMap<String, String> pluginConvo = new LinkedHashMap<>();
+
+            Main.log("AVENGERS, ASSEMBLE!");
+            pluginConvo.put("ChatPings", "Huh??");
+            pluginConvo.put("AdvancedTeleport", "What the?");
+            pluginConvo.put("HeadsPlus", "Hey, that's my line!!");
+            pluginConvo.put("SimplePets", "Oh.");
+            pluginConvo.put("KeepInvIndividual", "AAAAAAAA!!");
+
+            for (String plugin : pluginConvo.keySet()) {
+                Plugin validPlugin = Bukkit.getPluginManager().getPlugin(plugin);
+                if (validPlugin != null) {
+                    validPlugin.getLogger().info(pluginConvo.get(plugin));
+                }
+            }
+        } catch (NoClassDefFoundError ignored) {}
+    }
+}
